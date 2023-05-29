9389956b86490c84791604805529f1110177109f
FondantCn <fondntcn@@gemail.google>
2023-05-29 14:24:44 +0800

    更新基本文件

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<div>
+  <a href="">123</a>
+</div>
+</template>
+
+<style lang="scss" scoped>
+div{
+  a{
+    color: red;
+  }
+}
+</style>
