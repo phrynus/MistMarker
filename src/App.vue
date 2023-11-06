eef87bf91276af7fe963c1fddba245ad4948ee68
Ryxgmll <fondantcn@qq.com>
2023-11-06 14:48:03 +0800

    更换链接

--- src/App.vue
+++ src/App.vue
@@ -6,42 +6,42 @@ import mainBg from './components/mainBg.vue'
 // import {Ajax} from "./public/ajax.js";
 
 const mistmarker = {
-    mainBg: 'https://mistmarker.s3.bitiful.net/background/wallhaven-yxgmll.webp',
+    mainBg: 'https://o-web.s3.bitiful.net/mistmarker/background/background_wallhaven-yxgmll.png',
     engine: [
         {
             id: "baidu",
             name: "百度",
-            icon: "https://mistmarker.s3.bitiful.net/engine/baidu.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/baidu.png",
             url: "https://www.baidu.com/s?wd={data}"
         },
         {
             id: "google",
             name: "Google",
-            icon: "https://mistmarker.s3.bitiful.net/engine/google.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/google.png",
             url: "https://www.google.com/search?q={data}"
         },
         {
             id: "github",
             name: "GitHub",
-            icon: "https://mistmarker.s3.bitiful.net/engine/github.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/github.png",
             url: "https://github.com/search?q={data}"
         },
         {
             id: "codepen",
             name: "CodePen",
-            icon: "https://mistmarker.s3.bitiful.net/engine/codepen.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/codepen.png",
             url: "https://codepen.io/search/pens?q={data}"
         },
         {
             id: "gitee",
             name: "Gitee",
-            icon: "https://mistmarker.s3.bitiful.net/engine/gitee.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/gitee.png",
             url: "https://search.gitee.com/?q={data}"
         },
         {
             id: "juejin",
             name: "掘金",
-            icon: "https://mistmarker.s3.bitiful.net/engine/juejin.png",
+            icon: "https://o-web.s3.bitiful.net/mistmarker/engine/juejin.png",
             url: "https://juejin.cn/search?query={data}"
         }
     ],
